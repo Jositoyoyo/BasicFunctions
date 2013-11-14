54e0b664412a31275aeb95dd35333e9780c797ef
Jositoyoyo <jositoyoyo2@hotmail.com>
2013-11-14 22:54:06 +0100

    Tercer Cambio
    AMs fiunciones

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,9 @@ function onDeviceReady()
 {
 //listeners
 document.getElementById("results").innerHTML = "Ready"; 
-document.addEventListener("offline", onOffline, false);
+}
 document.addEventListener("menubutton", onMenuKeyDown, false);
 
-//funciones
-startWatch();
-}
 //funciones
 function onOffline()
 {
@@ -24,37 +21,24 @@ function onMenuKeyDown()
  document.getElementById("results").innerHTML = "Hola de nuevo, has pulsado el menu"; 
 }
 
-//acelometro
-//
-    function startWatch() {
-
-        // Update acceleration every 3 seconds
-        var options = { frequency: 3000 };
-
-        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+ // process the confirmation dialog result
+    function onConfirm(buttonIndex) {
+        alert('You selected button ' + buttonIndex);
     }
 
-    // Stop watching the acceleration
+    // Show a custom confirmation dialog
     //
-    function stopWatch() {
-        if (watchID) {
-            navigator.accelerometer.clearWatch(watchID);
-            watchID = null;
-        }
+    function showConfirm() {
+        navigator.notification.confirm(
+            'You are the winner!', // message
+             onConfirm,            // callback to invoke with index of button pressed
+            'Game Over',           // title
+            ['Restart','Exit']         // buttonLabels
+        );
     }
 
-    // onSuccess: Get a snapshot of the current acceleration
-    //
-    function onSuccess(acceleration) {
-        var element = document.getElementById('accelerometer');
-        element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
-                            'Acceleration Y: ' + acceleration.y         + '<br />' +
-                            'Acceleration Z: ' + acceleration.z         + '<br />' +
-                            'Timestamp: '      + acceleration.timestamp + '<br />';
-    }
 
-    // onError: Failed to get the acceleration
-    //
+
     function onError() {
         alert('onError!');
     }
